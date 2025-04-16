9100bbad4e89e81eb6aaca7cd993e8d146a07422
dkoncius <as.koncius@gmail.com>
2025-04-16 23:02:16 +0300

    github remove

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/banzai-tree/',
   plugins: [react()],
   resolve: {
     alias: {
